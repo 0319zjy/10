1053f91f10fe0bf44e52f5f870616b3ed4447732
0319zjy <3426385219@qq.com>
2025-12-31 11:14:19 +0800

    Update zjy11.py

--- zjy11.py
+++ zjy11.py
@@ -20,12 +20,12 @@ with st.sidebar:
 if page == "简介页面":
     st.title("企鹅分类器:penguin:")
     st.header('数据集介绍')
-    st.markdown('''**帕尔默群岛企鹅数据集是用于数据探索和数据可视化的一个出色的数据集，
-也可以作为机器学习入门练习。**
-该数据集是由 Gorman 等收集，并发布在一个名为 palmerpenguins 的 R 语言包，
+    st.markdown("""帕尔默群岛企鹅数据集是用于数据探索和数据可视化的一个出色的数据集，
+也可以作为机器学习入门练习。
+    该数据集是由 Gorman 等收集，并发布在一个名为 palmerpenguins 的 R 语言包，
 以对南极企鹅种类进行分类和研究。
-该数据集记录了 344 行观测数据，包含 3 个不同物种的企鹅：阿德利企鹅、巴布亚企
-鹅和帽带企鹅的各种信息。''')
+    该数据集记录了 344 行观测数据，包含 3 个不同物种的企鹅：阿德利企鹅、巴布亚企
+鹅和帽带企鹅的各种信息。""")
     st.header('三种企鹅的卡通图像')
     st.image('images/penguins.png')
 
@@ -43,12 +43,12 @@ elif page == "预测分类页面":
             sex = st.selectbox('性别', options=['雄性', '雌性'])
             bill_length = st.number_input('喙的长度（毫米）', min_value=0.0)
             bill_depth = st.number_input('喙的深度（毫米）', min_value=0.0)
-            flipper_length = st.number_input(' flipper（翅膀）的长度（毫米）', min_value=0.0)
+            flipper_length = st.number_input('翅膀的长度（毫米）', min_value=0.0)
             body_mass = st.number_input('身体质量（克）', min_value=0.0)
             submitted = st.form_submit_button('预测分类')
 
         # 初始化数据预处理格式中与岛屿相关的变量
-        island_dream, island_torgerson, island_biscoe = 0, 0, 0
+        island_dream, island_dream, island_torgerson = 0, 0, 0
         # 根据用户输入的岛屿数据更改对应的值
         if island == '比斯科岛':
             island_biscoe = 1
@@ -66,7 +66,7 @@ elif page == "预测分类页面":
             sex_male = 1
 
         format_data = [bill_length, bill_depth, flipper_length, body_mass,
-                       island_dream, island_torgerson, island_biscoe, sex_male, sex_female]
+                       island_dream, island_torgerson, island_biscoe,island_biscoe, sex_male, sex_female]
 
         # 使用pickle的load方法从磁盘文件反序列化加载一个之前保存的随机森林模型对象
         with open('rfc_model.pkl', 'rb') as f:
